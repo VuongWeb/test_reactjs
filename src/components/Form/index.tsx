802424444454b0608d7546a5ac3f4de280b883f4
VuongLV <vuonglvph15121@gmail.com>
2023-02-12 00:52:20 +0700

    feat: display components Form, Pagination

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Form = (props: Props) => {
   return (
-    <div>Form</div>
-  )
-}
+    <form action="">
+      <div className="col-span-6 sm:col-span-3">
+        <label
+          htmlFor="first-name"
+          className="block text-sm font-medium text-gray-700"
+        >
+          First name
+        </label>
+        <input
+          type="text"
+          name="first-name"
+          id="first-name"
+          autoComplete="given-name"
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+      </div>
+      <div className="col-span-6 sm:col-span-3">
+        <label
+          htmlFor="first-name"
+          className="block text-sm font-medium text-gray-700"
+        >
+          First name
+        </label>
+        <input
+          type="text"
+          name="first-name"
+          id="first-name"
+          autoComplete="given-name"
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+      </div>
+      <div className="col-span-6 sm:col-span-3">
+        <label
+          htmlFor="first-name"
+          className="block text-sm font-medium text-gray-700"
+        >
+          First name
+        </label>
+        <input
+          type="text"
+          name="first-name"
+          id="first-name"
+          autoComplete="given-name"
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+      </div>
+      <div className="col-span-6 sm:col-span-3">
+        <label
+          htmlFor="first-name"
+          className="block text-sm font-medium text-gray-700"
+        >
+          First name
+        </label>
+        <input
+          type="text"
+          name="first-name"
+          id="first-name"
+          autoComplete="given-name"
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+      </div>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
--- src/pages/ListUsers/index.tsx
+++ src/pages/ListUsers/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Form from "./../../components/Form/index.tsx";
 
 type Props = {};
 
-const ListUser = (props: Props) => {
+function ListUser(props: Props) {
   return (
-    <div>
+    <div className="w-80 mx-auto">
       <h2>ListUser</h2>
+      <Form />
     </div>
   );
-};
+}
 
 export default ListUser;
