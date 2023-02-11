28eaf1633b8a8d323f9c18ab8be77340e84e744f
VuongLV <vuonglvph15121@gmail.com>
2023-02-12 00:02:55 +0700

    feat: create router in project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './layouts/HomePage.tsx';
+import Users from './layouts/Users.tsx';
+import AddUser from './pages/AddUser/index.tsx';
+import EditUser from './pages/EditUser/index.tsx';
+import ListUsers from './pages/ListUsers/index.tsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<HomePage />}>
+          <Route path='/users' element={<Users />} >
+            <Route index element={<ListUsers />} />
+            <Route path='add' element={<AddUser />} />
+            <Route path='edit/:id' element={<EditUser />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ListUsers/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const ListUser = (props: Props) => {
+  return (
+    <div>
+      <h2>ListUser</h2>
+    </div>
+  );
+};
+
+export default ListUser;
