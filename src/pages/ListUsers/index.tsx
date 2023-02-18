c6d824ad32cf4f4d1153dde1ddd18fc64af652be
VuongLV <vuonglvph15121@gmail.com>
2023-02-18 14:23:15 +0700

    and test redux

--- src/pages/ListUsers/index.tsx
+++ src/pages/ListUsers/index.tsx
@@ -1,51 +1,28 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Table from "../../components/Table/index.tsx";
-import reducerUser from "../../redux/ReducerUsers.tsx";
-<<<<<<< HEAD
-import { deleteUser, listUsers } from "../../redux/userSlice.tsx";
-=======
-import { deleteUser, getListUsers } from "../../redux/userSlice.tsx";
->>>>>>> 879126c27e3087b6f7cdd8ad437b96c7be7edd83
+import { getListUsers } from "../../redux/userSlice.tsx";
 
 
 type Props = {};
 
 function ListUser(props: Props) {
-<<<<<<< HEAD
-  const usersList = useSelector((state: any) => state.users.items);
-=======
->>>>>>> 879126c27e3087b6f7cdd8ad437b96c7be7edd83
 
   const usersList = useSelector((state: any) => state.users.items)
   const dispatch = useDispatch();
 
-  const handdleRemove = (id) => {
-    // reducerUser(id);
-  };
-
   useEffect(() => {
-<<<<<<< HEAD
-    axios.get(' http://localhost:8000/users')
-      .then(function (response) {
-        dispatch(reducerUser(response.data,{type:"GET",payload:response.data}))
-      })
-      .catch(function (error) {
-        console.log('err get', error);
-      });
-=======
     axios({
       method: "get",
       url: "http://localhost:8000/users",
     }).then(function (response) {
       dispatch(getListUsers(response.data));
     });
->>>>>>> 879126c27e3087b6f7cdd8ad437b96c7be7edd83
-  }, []);
+  }, [usersList]);
 
   return (
     <div className="w-8/12 mx-auto">
@@ -53,7 +30,7 @@ function ListUser(props: Props) {
       <button className="float-right bg-green-600 py-3 px-5 text-white rounded-[15px]">
         <Link to={`/users/add`}>Add</Link>
       </button>
-      <Table users={usersList} action={handdleRemove} />
+      <Table users={usersList} />
       <ToastContainer />
     </div>
   );
--- src/redux/userSlice.tsx
+++ src/redux/userSlice.tsx
@@ -1,4 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const userSlice = createSlice({
@@ -8,17 +11,48 @@ const userSlice = createSlice({
   },
   reducers: {
         addUser:function(state:any,action){
-            state.items.push(action.payload)
+            state.items.push(action.payload);
+            axios.post('http://localhost:8000/users',action.payload)
+            .then(function (response) {
+              toast.success('Thêm thành công!')
+            })
+            .catch(function (error) {
+              toast.error('thêm lỗi')
+            });
         },
         deleteUser:function(state:any,action:any){
-            state.items = state.items.filter(item => item.id != action.payload.id)
+            axios.delete(`http://localhost:8000/users/${action.payload}`)
+            .then(function (response) {
+              toast.success('Xóa thành công!')
+            })
+            .catch(function (error) {
+              toast.error('Xóa lỗi')
+            });
         },
         getListUsers:function(state,action){
-          // state.items.push(action.payload);
+          state.items = action.payload;
+        },
+        getUsers:function(state,action){
+          
+        },
+        editUsers:function(state,action){
+          state.items.map((item:any)  =>{
+            if(item.id == Number(action.payload.id)){
+              item.name =action.payload.name;
+              item.phoneNumber =action.payload.phoneNumber;
+              item.email =action.payload.email;
+              axios.put(`http://localhost:8000/users/${action.payload.id}`,action.payload)
+            .then(function (response) {
+              toast.success('Update thành công!')
+            })
+            .catch(function (error) {
+              toast.error('Update lỗi')
+            });
+            }
+          })
         }
-
   }
 })
-export const { addUser,deleteUser ,getListUsers} = userSlice.actions
+export const { addUser,deleteUser ,getListUsers,editUsers} = userSlice.actions
 
 export default userSlice.reducer
